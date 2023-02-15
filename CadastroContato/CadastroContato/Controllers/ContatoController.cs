d9b479021ddfc5cf737bf6cd0d99fc428900f9c4
190488 <190488@facens.br>
2023-02-15 11:20:38 -0300

    Added static visual features

---
+++ CadastroContato/CadastroContato/Controllers/ContatoController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CadastroContato.Controllers
+{
+    public class ContatoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create() 
+        {
+            return View();
+        }
+        public IActionResult Update() 
+        {
+            return View();
+        }
+
+        public IActionResult DeleteConfirmation() 
+        {
+            return View();
+        }
+
+    }
+}
